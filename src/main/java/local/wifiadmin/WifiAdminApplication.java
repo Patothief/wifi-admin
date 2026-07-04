07595684c26c151573437733d53394a2bf24e646
Patothief <42675826+Patothief@users.noreply.github.com>
2026-07-04 12:31:39 +0200

    Initialize Spring Boot backend

---
+++ src/main/java/local/wifiadmin/WifiAdminApplication.java
@@ -0,0 +1,12 @@
+package local.wifiadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WifiAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WifiAdminApplication.class, args);
+    }
+}
